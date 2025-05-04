fdc304fe95ba1846c587b3886ee1a9b400cb4853
Baret90 <barisanremajatempel@gmail.com>
2025-05-04 09:34:57 +0700

    Add files via upload

--- login.js
+++ login.js
@@ -1,13 +1,1 @@
-
-function login() {
-    const username = document.getElementById("username").value.trim();
-    const password = document.getElementById("password").value.trim();
-    const users = JSON.parse(localStorage.getItem("users") || "[]");
-    const found = users.find(user => user.username === username && user.password === password);
-    if (found) {
-        localStorage.setItem("currentUser", username);
-        window.location.href = "index.html";
-    } else {
-        alert("Username atau password salah.");
-    }
-}
+console.log('Login script aktif');
--- saldo.js
+++ saldo.js
@@ -1,16 +1,1 @@
-
-document.addEventListener("DOMContentLoaded", function () {
-  const saldoElement = document.getElementById("saldo");
-  const finalSaldo = 18027000;
-  let current = 0;
-  const step = Math.ceil(finalSaldo / 100);
-
-  const interval = setInterval(() => {
-    current += step;
-    if (current >= finalSaldo) {
-      current = finalSaldo;
-      clearInterval(interval);
-    }
-    saldoElement.textContent = current.toLocaleString("id-ID");
-  }, 20);
-});
+console.log('Saldo script aktif');
